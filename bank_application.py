8a44d9733596612ddf6075457a581eb36aac86bd
seitzm97 <92923958+seitzm97@users.noreply.github.com>
2022-03-25 10:02:03 +0100

    Update bank_application.py

--- bank_application.py
+++ bank_application.py
@@ -12,27 +12,28 @@ import sys
 class BankApp():    
     def __init__(self):
         self.acc_creator()
-    
-    def selecter():
-        acc = input("Please type the accounts name you'd like to select: ")
-        accounts[acc].action()
         
-    def menu(self,option_1, option_2, option_3, option_4, option_5):
+    def menu(self,option_1, option_2, option_3, option_4, option_5, option_6):
         print(20*"-","MENU",20*"-")
         print("1. ", option_1)
         print("2. ", option_2)
         print("3. ", option_3)
         print("4. ", option_4)
         print("5. ", option_5)
+        print("6. ", option_6)
         print(46*"-")
         self.choice = int(input("Please choose an option."))
         
     def owner(self, name, address, phone, date_of_birth):
         bk.BankAccount.owner(self, name, address, phone, date_of_birth)
     
+    def selecter():
+        acc = input("Please type the accounts name you'd like to select: ")
+        accounts[acc].action()
+    
     def action(self):
         while True:
-            self.menu("Deposit","Withdraw","Create another account","Delete/Close the account","Exit")
+            self.menu("Deposit","Withdraw","Create another account","Delete/Close the account","Tax report", "Exit")
             if self.choice == 1:
                 amount = int(input("Please enter the amount you would like to deposit."))
                 if self.account_type == "Bank account" or self.account_type == "Youth account":
@@ -54,15 +55,18 @@ class BankApp():
                 dict_key = input("Please enter the accounts name. ")
                 accounts.pop(dict_key)
             if self.choice == 5:
+                self.taxreporter()
+            if self.choice == 6:
                 print("Leaving the Menu")
                 del self.choice
                 break
+            
             
     def deleter(self):
         del self.name, self.account_type, self.choice,self.rate, self.address, self.age, self.balance, self.birthdate, self.date_of_birth, self.iban, self.opening_date, self.phone
     
     def acc_creator(self):
-        self.menu("Bank account", "Saving account", "Youth account","Exit", "--")
+        self.menu("Bank account", "Saving account", "Youth account","Exit", "--", "--")
         if self.choice == 4:
             sys.exit("Exitiing the application")
         self.name = input("What is your name. ")
@@ -80,6 +84,12 @@ class BankApp():
             ya.YouthAccount.__init__(self, self.name, self.address, self.phone, self.date_of_birth)
         else:
             pass
+    
+    def taxreporter(self):
+        print("Tax report 2022 for fiscal year 2021")
+        print("**",self.account_type,"**", self.balance, "CHF")
+        
+        
         
 if __name__ == '__main__':
     acc_name = str(input("You don't have an account yet.Enter an account name: "))
