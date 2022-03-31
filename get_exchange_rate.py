2eb9aa6e91531ee622bb90fa646b57403ffce3d0
seitzm97 <92923958+seitzm97@users.noreply.github.com>
2022-03-31 11:36:33 +0200

    Update get_exchange_rate.py
    Did some clean up.

--- get_exchange_rate.py
+++ get_exchange_rate.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 31 10:12:38 2022
-
-@author: Seitz
-"""
-
 from urllib.request import urlopen
 import json
 
@@ -16,11 +9,7 @@ class get_exchange_rate():
         self.url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/chf.json"
         self.page = urlopen(self.url)
         self.data_json = json.loads(self.page.read())
-        del self.data_json["date"]
-        del self.page
-        del self.url
         self.exchange_rate = self.data_json["chf"][currency]
-        del self.data_json
         return self.exchange_rate
     
 
@@ -31,5 +20,6 @@ if __name__ == '__main__':
     exchange_rate = get_exchange_rate(currency)
     print("The current exchange rate from CHF to", currency.upper(),"is",round(exchange_rate.exchange_rate,2))
     del currency
+    del exchange_rate
 else:
     pass
